008604ddf3116a77b58a955753220b80bff144bd
RyanzpLee <lzpryan@gmail.com>
2020-08-26 00:44:00 +0100

    Php update

--- contact.php
+++ contact.php
@@ -9,19 +9,12 @@ $name = $_POST['name']; // required
 $email_from = $_POST['email']; // required
 $message = $_POST['message']; // required
 
-$email_message = "Form details below.\n\n";
-
-$email_message .= "Name: ".clean_string($name)."\n";
-$email_message .= "Email: ".clean_string($email_from)."\n";
-$email_message .= "Message: ".clean_string($message)."\n";
-$recipient = "hi@ryanzplee.com";    
-
 // create email headers
-$headers = 'From: '.$recipient."\r\n".
-'Reply-To: '.$recipient."\r\n" .
+$headers = 'From: '.$name."\r\n".
+'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers); 
-header("Location:http://www.ryanzplee.com/");  
+mail($email_to, $email_subject, $email_message, $headers) or die("Error!"); 
+
 
 echo "Thank you for your message!" . " -" . "<a href="index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
